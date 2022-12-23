fe614b9ad029ee4a86683853479db1099a30dc34
Marcelo <dev@marcelorock.com>
2022-12-23 03:45:29 -0400

    previsao do tempo

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,7 @@
                 }
             })
             this.input.addEventListener('blur', e=>{
-                setTimeout(() => this.ulCidades.classList.remove('ativo'), 100)
+                setTimeout(() => this.ulCidades.classList.remove('ativo'), 300)
             })
         }
 
